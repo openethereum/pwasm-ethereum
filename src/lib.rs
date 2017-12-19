d8754955f5a62abecfd486f0eaa836a025906125
NikVolf <nikvolf@gmail.com>
2017-12-20 01:29:18 +0300

    no-std attr

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 //! Externs library to interact with Ethereum-like network
 
+
+#![cfg_attr(not(feature="std"), no_std)]
+
 extern crate pwasm_std;
 extern crate parity_hash as hash;
 extern crate bigint;
